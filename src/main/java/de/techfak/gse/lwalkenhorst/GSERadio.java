7dd4085e7b1ba3b8a2a512818c9ec43f4a8cf68c
lwalkenhorst <lwalkenhorst@techfak.uni-bielefeld.de>
2019-11-01 18:55:54 +0100

    Add functionality to start parameter 'directory name' refs #1707

--- src/main/java/de/techfak/gse/lwalkenhorst/GSERadio.java
+++ src/main/java/de/techfak/gse/lwalkenhorst/GSERadio.java
@@ -1,5 +1,7 @@
 package de.techfak.gse.lwalkenhorst;
 
+import java.io.File;
+
 public final class GSERadio {
 
     private GSERadio() {
@@ -7,6 +9,12 @@ public final class GSERadio {
 
     public static void main(final String... args) {
         System.out.println("Hello lwalkenhorst!");
+        try {
+            MusicReader musicReader = new MusicReader(args.length == 1 ? args[0] : null);
+            File[] musicFiles = musicReader.getMusicFiles();
+        } catch (NoMusicFileFoundException exception) {
+            System.err.println(exception.getMessage());
+        }
     }
 
 }
